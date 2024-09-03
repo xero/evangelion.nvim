e7e2113092b775559d517a41c9a7f56549ce80fb
xero <x@xero.style>
2024-09-03 18:13:39 -0400

    feat(lua): 𝓡𝓮𝔀𝓻𝓲𝓽𝓮 𝓕𝓮𝓪𝓽 𝓣𝓻𝓪𝓷𝓼𝓹𝓪𝓻𝓮𝓷𝓽𝓬𝔂 📝

--- README.md
+++ README.md
@@ -31,26 +31,35 @@ using `lazy`
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme evangelion")
+    require("evangelion").setup({
+      transparent = false,
+      unit = "01",
+      aggressive_spell = false,
+    })
+    vim.cmd.colorscheme("evangelion")
   end,
-},
+}
 ```
 
 ## customization
 
-this theme is built with `lush.nvim`, so customizationis are quite easy.
+this theme is built with `lush.nvim`, so customizations are quite easy.
 
-open `lua/lush_theme/evangelion.lua` and execute `:Lushify`. then adjust colors to suit your taste with real-time feedback. for the best experience, i suggest disabling LSP and enabeling a colorizer like [nvim-highlight-colors](https://github.com/brenoprata10/nvim-highlight-colors) or [NvChad-colorizer](https://github.com/NvChad/nvim-colorizer.lua).
+open [lush_theme/evangelion.lua](lush_theme/evangelion.lua) and execute `:Lushify`. then adjust colors in [lua/evangelion/unit01.lua]([lua/evangelion/unit01.lua) to suit your taste with real-time feedback. for the best experience, i suggest disabling LSP and enabeling a colorizer like [nvim-highlight-colors](https://github.com/brenoprata10/nvim-highlight-colors) or [NvChad-colorizer](https://github.com/NvChad/nvim-colorizer.lua).
 
 lots more details on using `lush` in their repo: [rktjmp/lush.nvim](https://github.com/rktjmp/lush.nvim).
 
 ## building
 
-lush is cool and all, but loading the raw lua theme version takes 25+ ms...
+after customizations, use the [build.sh](build.sh) script in the root directory to create release versions of the theme.
 
-**ermergerd mah startup time!!1!#@~**
+* running `./build.sh lua` with generate `lua/evangelion/theme.lua`
+* running `./build.sh vim` with generate `colors/evangelion.vim`
 
-so i suggest exporting it as a real vim colorscheme. simply run the
-[build.sh](build.sh) script in the root directory and `colors/evangelion.vim` and `colors/evangelion.lua` will be
-`{cre,upd}ated`. more info in the [lush without lush guide](https://github.com/rktjmp/lush.nvim/blob/main/BUILD.md).
+more info in the [lush without lush guide](https://github.com/rktjmp/lush.nvim/blob/main/BUILD.md).
 
+> **_NOTE_** neovim will source `colors/evangelion.vim` if it exists and ignore the lua version. after generating evangelion.vim, switch to the `vim` branch and checkout the newly created file. e.g. `git add colors/evangelion.vim; git commit -m "update"; git checkout vim; git checkout dev colors/evangelion.vim`. the same workflow should apply to the `main` branch using `git checkout dev lua/evangelion/theme.lua`
+
+## contributing
+
+pr's to this repo are welcome. just make sure they target this `dev` branch.
--- build.sh
+++ build.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 shopt -s extglob
-
-getintherobotshinji() {
-cat << x0
-$1 x@xero.style    _            ____
-$1 https://x-e.ro : \\           |   \\    .
-$1                |  \\ .        |    :   |\\            /\\
-$1             .  |   :|\\__     |    |   | \\          /  \\
-$1.            |\\ |   |! \\ \\    |    |   | |\\        /   /
-$1\\"-.______   | \\:   ||\\ \\ \\   |    |   | | \\      /   /
-$1 \\_       "-_|  |\\  || \\ \\/   |    |___| ! |\\____/  _/-. /\\
-$1   "-_   ____:  |_\\ ||  \\/  ___\\  __  _//  | |  ___ \\---" /
-$1      \\  \\   |  _____,  /___\\___\\/ / /   \\_! |  // _/  / /
-$1    ___\\_ \\__|  |    | __. _/____ / /     /  > // /    \\/
-$1  //_________|  /    |/  |/  \\__// /     /  /_/ \\/
-$1             | /     |   :      | /     /__/
-$1             |/                 |/   E V A N G E L I O N
-
-x0
-}
-
-# setup shipwright for vim
-cat << x0 > ./shipwright_build.lua
+case "$@" in
+#-------------------------------------------------[ build vim ]
+vim)
+	# setup shipwright for vim
+	cat <<-x0 > ./shipwright_build.lua
 local colorscheme = require("lush_theme.evangelion")
 local lushwright = require("shipwright.transform.lush")
 
@@ -29,12 +12,24 @@ run(colorscheme,
   lushwright.to_vimscript,
   {overwrite, "colors/evangelion.vim.tmp"}
 )
-x0
-nvim --headless +Shipwright +qa
+	x0
+	nvim --headless +Shipwright +qa
 
-# create colorscheme
-cat << x0 > ./colors/evangelion.vim
-$(getintherobotshinji '"')
+	# create colorscheme
+	cat <<-x0 > ./colors/evangelion.vim
+" x@xero.style    _            ____
+" https://x-e.ro : \\           |   \\    .
+"                |  \\ .        |    :   |\\            /\\
+"             .  |   :|\\__     |    |   | \\          /  \\
+".            |\\ |   |! \\ \\    |    |   | |\\        /   /
+"\\"-.______   | \\:   ||\\ \\ \\   |    |   | | \\      /   /
+" \\_       "-_|  |\\  || \\ \\/   |    |___| ! |\\____/  _/-. /\\
+"   "-_   ____:  |_\\ ||  \\/  ___\\  __  _//  | |  ___ \\---" /
+"      \\  \\   |  _____,  /___\\___\\/ / /   \\_! |  // _/  / /
+"    ___\\_ \\__|  |    | __. _/____ / /     /  > // /    \\/
+"  //_________|  /    |/  |/  \\__// /     /  /_/ \\/
+"             | /     |   :      | /     /__/
+"             |/                 |/   E V A N G E L I O N
 set background=dark
 hi! clear
 
@@ -46,38 +41,46 @@ let colors_name="evangelion"
 let g:colors_name="evangelion"
 
 $(sed 's/^highlight/hi/' ./colors/evangelion.vim.tmp | sort)
-x0
-echo "evangelion vim build complete"
+	x0
+	rm ./colors/*.tmp ./shipwright_build.lua
+	echo "evangelion vim build complete"
+	;;
 
-# setup shipwright for lua
-cat << x0 > ./shipwright_build.lua
+#-------------------------------------------------[ build lua ]
+lua)
+	# setup shipwright for lua
+	cat <<-x0 > ./shipwright_build.lua
 local colorscheme = require("lush_theme.evangelion")
 local lushwright = require("shipwright.transform.lush")
 
 run(colorscheme,
   lushwright.to_lua,
-  {patchwrite, "colors/evangelion.lua.tmp", "-- PATCH_OPEN", "-- PATCH_CLOSE"})
-x0
-# create base file
-cat << x0 > ./colors/evangelion.lua.tmp
-$(getintherobotshinji "--")
-local colors = {
+  {patchwrite, "lua/evangelion/theme.lua.tmp", "-- PATCH_OPEN", "-- PATCH_CLOSE"})
+	x0
+	# create base file
+	cat <<-x0 > ./lua/evangelion/theme.lua.tmp
+X = {}
+---@return table
+X.build = function()
+	local theme = {
 -- PATCH_OPEN
--- group data will be inserted here
 -- PATCH_CLOSE
-}
-
-vim.cmd("highlight clear")
-vim.cmd("set background=dark")
-vim.cmd("set t_Co=256")
-vim.cmd("let g:colors_name='evangelion'")
-vim.cmd("let colors_name='evangelion'")
-
-for group, attrs in pairs(colors) do
-  vim.api.nvim_set_hl(0, group, attrs)
+	}
+	if require("evangelion.config").opts.transparent then
+		theme["Normal"].bg = "none"
+		theme["StatusLine"].bg = "none"
+	end
+	return theme
 end
-x0
-nvim --headless +Shipwright +qa
-awk '/-- PATCH/{getline}{print}' ./colors/evangelion.lua.tmp > ./colors/evangelion.lua
-rm ./colors/*.tmp ./shipwright_build.lua
-echo "evangelion lua build complete"
+return X
+	x0
+	nvim --headless +Shipwright +qa
+	awk '/-- PATCH/{getline}{print}' ./lua/evangelion/theme.lua.tmp > ./lua/evangelion/theme.lua
+	rm ./lua/evangelion/*.tmp ./shipwright_build.lua
+	echo "evangelion lua build complete"
+	;;
+*)
+	echo "usage: ./build.sh (vim|lua)"
+	;;
+esac
+#-------------------------------------------------------[ eof ]
